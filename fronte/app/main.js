8d542ae03c2f2b57558d6c398b60315f481da9be
ori davidi <oridavidi100@gmail.com>
2021-11-04 13:33:41 +0200

    STYLE
    CSS

--- fronte/app/main.js
+++ fronte/app/main.js
@@ -1,14 +1,22 @@
 
 import "./styles/public/style.css"
 document.getElementById("submit").addEventListener("click",shortApi)
-
+ document.getElementById("statistic").addEventListener("click",showData)
+let id ;
+let userName;
 async function shortApi(){
+    userName=document.getElementById("userName").value
     let longUrl=document.getElementById("url_input").value
+    document.getElementById("short").style.display ="block"
    try{
     let response= await axios.post("http://localhost:3000/api",{
+        "userName":userName,
         "longUrl":longUrl
     })
-    document.getElementById("short").innerText="your new url   :"+(response.data)
+    let p=document.createElement("p")
+    p.innerText="your new url:"+"  "+(response.data.shortUrl)
+    document.getElementById("short").appendChild(p)
+     id=response.data.id
 }  
 catch(error){
     console.log(error.response.data.error)
@@ -16,3 +24,17 @@ catch(error){
 }
 }
 
+
+async function showData(){
+        console.log(id)
+        let response = await axios.get(`http://localhost:3000/api/statistic/${id}/${userName}`)
+        let data=(response.data)
+        console.log(response.data.date)
+        console.log(response.data.redirectCount)
+        let div=document.getElementById("short")
+        let p = document.createElement("p")
+        p.innerText=(`Date:${data.date}  
+        redirectCount:${data.redirectCount}`)
+        div.appendChild(p)
+    
+}  
